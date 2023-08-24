5321749875795ae4cad5c1fd56491c2f32743779
Musthafa Faishal <musthafafaishal@gmail.com>
2023-08-24 19:11:05 +0700

    refactor: update the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,58 @@
-import React from 'react';
-import logo from './assets/images/logo.svg';
-import './assets/styles/App.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { asyncPreloadSuccess } from './states/isPreload/action';
+// import { asyncUnsetAuthUser } from './states/authUser/action';
+import Loading from './components/Loading';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import LoginPage from './pages/LoginPage';
 
 function App() {
+  const { authUser = null, isPreload = false } = useSelector(
+    (states) => states,
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncPreloadSuccess());
+  }, [dispatch]);
+
+  // const onSignOut = () => {
+  //   dispatch(asyncUnsetAuthUser());
+  // };
+
+  if (isPreload) {
+    return null;
+  }
+
+  if (authUser === null) {
+    return (
+      <>
+        <Loading />
+        <Header className />
+        <main className=" bg-white m-auto h-screen max-w-4xl">
+          <Routes>
+            <Route
+              path="/"
+              element={<LoginPage />}
+            />
+            <Route
+              path="/login"
+              element={<LoginPage />}
+            />
+          </Routes>
+        </main>
+        <Footer />
+      </>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Loading />
+      <Header />
+    </>
   );
 }
 
