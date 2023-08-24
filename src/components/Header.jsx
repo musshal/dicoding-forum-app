1dde691e16a208c1f522e54d3fee221005a5f988
Musthafa Faishal <musthafafaishal@gmail.com>
2023-08-24 19:34:00 +0700

    feat: update link to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="fixed top-0 bg-slate-700 w-full p-4">
       <h1 className="text-white uppercase tracking-wide text-2xl">
-        Dicoding Forum App
+        <Link to="/">Dicoding Forum App</Link>
       </h1>
     </header>
   );
