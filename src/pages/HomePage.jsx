45846c8d9cbdb4b83a597cb238a3b55765882ca9
Musthafa Faishal <musthafafaishal@gmail.com>
2023-08-24 20:29:37 +0700

    feat: create home page and leaderboard page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <section>
+      <h2>Kategori populer</h2>
+    </section>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/LeaderboardPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LeaderboardPage() {
+  return (
+    <section>
+      <h2>Klasmen Pengguna Aktif</h2>
+    </section>
+  );
+}
+
+export default LeaderboardPage;
