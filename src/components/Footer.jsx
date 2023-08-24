fc8d878eeffcf866219be0c7802b051de657c772
Musthafa Faishal <musthafafaishal@gmail.com>
2023-08-24 20:29:19 +0700

    refactor: update button to link element

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,20 +7,22 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <footer className="fixed bottom-0 shadow-inner w-full flex justify-center gap-10 bg-white text-gray-600">
-      <button
+      <Link
+        to="/threads"
         type="button"
         className="inline-flex flex-col p-2 items-center justify-center group"
       >
         <HiOutlineChatBubbleLeftRight size={25} />
         <span>Threads</span>
-      </button>
-      <button
+      </Link>
+      <Link
+        to="/leaderboards"
         type="button"
         className="inline-flex flex-col p-2 items-center justify-center group"
       >
         <MdOutlineLeaderboard size={25} />
         <span>Leaderboards</span>
-      </button>
+      </Link>
       <Link
         to="/login"
         className="inline-flex flex-col p-2 items-center justify-center group"
