24cbffaa4a9649cb30948e61daf6c1cf1ce3f242
Musthafa Faishal <musthafafaishal@gmail.com>
2023-09-09 11:30:34 +0700

    feat: create new page

---
+++ src/pages/AddThreadPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import ThreadInput from '../components/ThreadInput';
+import { asyncAddThread } from '../states/threads/action';
+
+function AddThreadPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const onCreate = ({ title, category, body }) => {
+    dispatch(asyncAddThread({ title, category, body }));
+    navigate('/');
+  };
+
+  return (
+    <section className="flex flex-col gap-10">
+      <h2 className="text-2xl font-semibold">Buat Diskusi Baru</h2>
+      <ThreadInput onCreate={onCreate} />
+    </section>
+  );
+}
+
+export default AddThreadPage;
