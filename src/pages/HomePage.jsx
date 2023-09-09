52da40bbcad4f0ffd53b4a124851a24326704e83
Musthafa Faishal <musthafafaishal@gmail.com>
2023-09-09 11:30:50 +0700

    refactor: add link to add thread page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { IoIosAddCircle } from 'react-icons/io';
+import { Link } from 'react-router-dom';
 import CategoryList from '../components/CategoryList';
 import ThreadList from '../components/ThreadList';
 import { asyncPopulateUsersAndThreads } from '../states/shared/action';
@@ -27,14 +29,26 @@ function HomePage() {
   );
 
   return (
-    <section>
-      <h2 className="mb-2">Kategori populer</h2>
-      <CategoryList categories={categories} />
-      <h2 className="my-7 text-2xl font-semibold tracking-normal">
-        Diskusi Tersedia
-      </h2>
-      <ThreadList threads={threadsList} />
-    </section>
+    <>
+      <section>
+        <h2 className="mb-2">Kategori populer</h2>
+        <CategoryList categories={categories} />
+        <h2 className="my-7 text-2xl font-semibold tracking-normal">
+          Diskusi Tersedia
+        </h2>
+        <ThreadList threads={threadsList} />
+      </section>
+      {authUser ? (
+        <Link
+          to="/threads/add"
+          className="fixed bottom-0 right-0 mb-20 mr-10"
+        >
+          <IoIosAddCircle size={50} />
+        </Link>
+      ) : (
+        ''
+      )}
+    </>
   );
 }
 
