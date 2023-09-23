93e1d72e998a28640f0750d3967d5f540695da28
Musthafa Faishal <musthafafaishal@gmail.com>
2023-09-23 15:03:23 +0700

    refactor: implement react toastify

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
-import { BrowserRouter } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import store from './states';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './states';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
+        <ToastContainer />
         <App />
       </React.StrictMode>
     </BrowserRouter>
