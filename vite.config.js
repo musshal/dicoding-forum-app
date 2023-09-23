3e6d73091364575baff2158e3f185ac05228af19
Musthafa Faishal <musthafafaishal@gmail.com>
2023-09-23 16:41:20 +0700

    feat: create vite config

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+  },
+  server: {
+    port: 3000,
+  },
+});
